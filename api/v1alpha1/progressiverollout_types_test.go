8f549375116713d5c680e5782df3dadd7afafe25
Matteo Ruina <matteo.ruina@gmail.com>
2021-02-16 10:35:15 +0100

    Add set condition tests

--- api/v1alpha1/progressiverollout_types_test.go
+++ api/v1alpha1/progressiverollout_types_test.go
@@ -14,6 +14,7 @@ var _ = Describe("ProgressiveRollout Status", func() {
 	Context("Status", func() {
 
 		Describe("SetCondition", func() {
+
 			It("should add a new conditions", func() {
 				status := &ProgressiveRolloutStatus{}
 				c := status.NewCondition(CompletedType, metav1.ConditionTrue, "Completed")
@@ -21,6 +22,27 @@ var _ = Describe("ProgressiveRollout Status", func() {
 				Expect(status.Conditions[0]).To(Equal(c))
 				Expect(len(status.Conditions)).To(Equal(1))
 			})
+
+			It("should replace a pre-existing conditions", func() {
+				status := &ProgressiveRolloutStatus{}
+				empty := status.NewCondition(CompletedType, metav1.ConditionTrue, "")
+				status.SetCondition(&empty)
+				nonEmpty := status.NewCondition(CompletedType, metav1.ConditionTrue, "Completed")
+				status.SetCondition(&nonEmpty)
+				Expect(status.Conditions[0]).To(Equal(nonEmpty))
+				Expect(len(status.Conditions)).To(Equal(1))
+			})
+
+			It("should not update the condition if it already exists and has the same status and reason.", func() {
+				status := &ProgressiveRolloutStatus{}
+				first := status.NewCondition(CompletedType, metav1.ConditionFalse, "Failed")
+				status.SetCondition(&first)
+				second := status.NewCondition(CompletedType, metav1.ConditionFalse, "Failed")
+				status.SetCondition(&second)
+				Expect(status.Conditions[0]).To(Equal(first))
+				Expect(len(status.Conditions)).To(Equal(1))
+			})
+
 		})
 	})
 })
