a70f58810b33088ce67082b7e5009782f0e1a7c1
Matteo Ruina <matteo.ruina@gmail.com>
2021-04-14 14:40:17 +0200

    Update tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,6 @@
 // +build tools
 
-// This file ensures tool dependencies are kept in sync.  This is the
+// This file ensures tool dependencies are kept in sync. This is the
 // recommended way of doing this according to
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 // To install the following tools at the version used by this repo run:
