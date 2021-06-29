90a2bf37f51efa02e8a7281cd482478ef1233cb5
Matteo Ruina <matteo.ruina@gmail.com>
2021-06-29 14:47:40 +0200

    Always log the progressive sync object

--- controllers/progressivesync_controller.go
+++ controllers/progressivesync_controller.go
@@ -360,7 +360,7 @@ func (r *ProgressiveSyncReconciler) setSyncedAtAnnotation(ctx context.Context, a
 
 // reconcileStage reconcile a ProgressiveSyncStage
 func (r *ProgressiveSyncReconciler) reconcileStage(ctx context.Context, ps syncv1alpha1.ProgressiveSync, stage syncv1alpha1.ProgressiveSyncStage) (syncv1alpha1.ProgressiveSync, reconcile.Result, error) {
-	log := r.Log.WithValues("stage", stage.Name)
+	log := r.Log.WithValues("stage", stage.Name).WithValues("progressivesync", ps.Name)
 
 	// Get the clusters to update
 	clusters, err := r.getClustersFromSelector(ctx, stage.Targets.Clusters.Selector)
