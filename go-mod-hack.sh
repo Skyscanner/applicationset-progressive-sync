d16d18addf07a88b5f4f9d5f91a46ec45a89c5c9
Matteo Ruina <matteo.ruina@gmail.com>
2021-10-15 14:13:26 +0200

    Add root files

---
+++ go-mod-hack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# See https://github.com/argoproj/argo-cd/issues/4055
+set -euo pipefail
+
+VERSION=${1#"v"}
+if [[ -z "$VERSION" ]]; then
+	echo "Must specify version!"
+	exit 1
+fi
+
+FILES=$(
+	curl -sS https://raw.githubusercontent.com/kubernetes/kubernetes/v"${VERSION}"/go.mod |
+		sed -n 's|.*k8s.io/\(.*\) => ./staging/src/k8s.io/.*|k8s.io/\1|p'
+)
+
+while read -r MOD; do
+	V=$(
+		go mod download -json "${MOD}@kubernetes-${VERSION}" |
+			sed -n 's|.*"Version": "\(.*\)".*|\1|p'
+	)
+	go mod edit "-replace=${MOD}=${MOD}@${V}"
+done <<<"$FILES"
+
+go get "k8s.io/kubernetes@v${VERSION}"
